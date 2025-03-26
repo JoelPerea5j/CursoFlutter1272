076530549f6a7efc58596bcb1bcf7bfaf65dd166
JOEL_ALBERTO_PEREA_CASTORENA <a.22308051281272@cbtis128.edu.mx>
2025-03-26 19:39:10 +0000

    Ejemplo de  List Tilte

--- Z_Recursos/Leeme.md
+++ Z_Recursos/Leeme.md
@@ -1,6 +1,10 @@
 ## Ejemplo Card
 ![La tarjeta](Card.png)
 
-## Ejemplo 
+## Ejemplo  de Dialogo
 ![La tarjeta](Dialogo_R.png)
 ![La tarjeta](Dialogo.png)
+
+## Ejemplo de List Title
+![La tarjeta](List_Tilte.png)
+![La tarjeta](List_Tilte_R.png)
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 //import 'Mis_Widgets/Mis_Tarjetas.dart';
-import 'Mis_Widgets/Dialogo_Acerca_De.dart';
+//import 'Mis_Widgets/Dialogo_Acerca_De.dart';
+import 'Mis_Widgets/Acerca_de_titulo_de_lista.dart';
 
 const Color darkBlue = Color(0xFF12202F);
 void main() => runApp(MiTarjeta());
@@ -20,7 +21,7 @@ class MiTarjeta extends StatelessWidget {
           centerTitle: true,
         ),
         //body: const LaTarjeta(),
-        body: const DialogoAcerca(),
+        body: const AcercaLista(),
       ),
     );
   }
